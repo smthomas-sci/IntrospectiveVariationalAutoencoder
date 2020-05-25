84fc91143037c28415f9a6c90165852554eb9b0d
smthomas-sci <simon.thomas@uq.edu.au>
2020-05-25 12:54:44 +1000

    Added style blocks etc.

--- introvae/layers.py
+++ introvae/layers.py
@@ -2,12 +2,14 @@
 Custom Layers for building a Variational Autoencoder
 
 Author: Simon Thomas
-DateL 22-May-2020
+Date: 22-May-2020
 
 """
+import numpy as np
 import keras.backend as K
 from keras.layers import Lambda, BatchNormalization, Add, LeakyReLU, Conv2D,\
-                         UpSampling2D, Input, Flatten, Dense, Reshape
+                         UpSampling2D, Input, Flatten, Dense, Reshape, Layer, \
+                         Cropping2D
 
 
 def tRGB(x, block):
@@ -34,7 +36,11 @@ def fRGB(x, rgb, filters, block):
     t = Conv2D(filters, (1, 1), strides=(1, 1), padding="same", name=f"block_{block}_fRGB")(rgb)
     t = BatchNormalization(name=f"block_{block}_BN_fRGB")(t)
     t = LeakyReLU(0.2)(t)
-    return t if x is None else x + t
+    if x is not None:
+        x = Conv2D(filters, (3, 3), strides=(1, 1), padding="same", name=f"block_{block}_fRGB_res")(x)
+        x = Add()([x, t])
+        return x
+    return t
 
 def conv_block(x, filters, block, down=True, residual=True):
     """
@@ -101,20 +107,108 @@ def add_vae_layer(x, z_dim):
     z = Lambda(sampling, name='z')([mu, log_var])
     return z, mu, log_var
 
-def add_base_layer(z, base_dim):
+def add_base_layer(z=None, base_dim=(4, 4, 512), style=False):
     """
     Adds the base layer of the generator and returns the lowest
-    convolutional feature tensor.
+    convolutional feature tensor. All inputs need to be specified.
 
     :param z: the latent vector z
     :param base_dim: the shape of the lowest convolutional feature e.g. (4x4x512)
-    :return: x
+    :param style:  indicates whether a style network with constant input
+    :return: x OR (constant_input, x)
     """
-    x = Dense(K.prod(base_dim), name="base_flatten")(z)
-    x = BatchNormalization(name="base_BN")(x)
-    x = LeakyReLU(0.2)(x)
-    x = Reshape(base_dim, name="base_Reshape")(x)
+    if style:
+        # Constant Start - 4x4x512
+        constant_input = Input(tensor=K.constant([[1]]), name="constant_input")
+        x = Dense(np.product(base_dim))(constant_input)
+        x = Reshape(base_dim)(x)
+        x = BatchNormalization(name="base_BN")(x)
+        x = LeakyReLU(0.2)(x)
+        return constant_input, x
+    else:
+        x = Dense(np.prod(base_dim), name="base_flatten")(z)
+        x = BatchNormalization(name="base_BN")(x)
+        x = LeakyReLU(0.2)(x)
+        x = Reshape(base_dim, name="base_Reshape")(x)
+        return x
+
+def add_mapping_layers(z, z_dim, n_layers):
+    """
+    Add a mapping network to the graph
+    :param z: the input tensor
+    :param z_dim: the dimension of z
+    :param n_layers: the number of layers in mapping network
+    :return:
+    """
+    x = z
+    for l in range(n_layers):
+        x = Dense(z_dim, name=f"mapping_network_{l}")(x)
+        x = LeakyReLU(0.2, name=f"mapping_network_relu_{l}")(x)
     return x
+
+
+def style_block(filters, input_tensor, style_tensor, noise_image, block_num):
+    """
+    Creates a style block with residual connections.
+    :param filters: the number of filters for this block
+    :param input_tensor: the tensor to modulate e.g. 4x4x512 -> 8x8x256 and upwards
+    :param style_tensor: the style tensor to modulate with i.e. w1, w2, w3 etc.
+    :param noise_image: the noise image for this scale e.g. 8x8x1, 16x16x1 etc.
+    :param block_num: the depth of the network
+    :return: x - the tensor to return
+    """
+    beta = Dense(filters, name="block_{0}_beta1".format(block_num))(style_tensor)
+    beta = Reshape([1, 1, filters], name="block_{0}_beta1_reshape".format(block_num))(beta)
+    gamma = Dense(filters, name="block_{0}_gamma1".format(block_num))(style_tensor)
+    gamma = Reshape([1, 1, filters], name="block_{0}_gamma1_reshape".format(block_num))(gamma)
+
+    # Learn a bias for the noise at this level
+    noise = Conv2D(filters=filters,
+                   kernel_size=1,
+                   padding='same',
+                   kernel_initializer='he_normal',
+                   name="block_{0}_noise_bias1".format(block_num)
+                   )(noise_image)
+
+    # Phase 1
+    x = UpSampling2D(name="block_{0}_UpSample".format(block_num))(input_tensor)
+    x = Conv2D(filters=filters,
+               kernel_size=3,
+               padding='same',
+               kernel_initializer='he_normal',
+               name="block_{0}_decoder_conv1".format(block_num)
+               )(x)
+    x = AdaInstanceNormalization(name="block_{0}_AdaIN_1".format(block_num))([x, beta, gamma])
+    x = Add()([x, noise])
+    x = LeakyReLU(name="block_{0}_decoder_LRelu1".format(block_num))(x)
+
+    # Phase 2
+    beta = Dense(filters, name="block_{0}_beta2".format(block_num))(style_tensor)
+    beta = Reshape([1, 1, filters], name="block_{0}_beta2_reshape".format(block_num))(beta)
+    gamma = Dense(filters, name="block_{0}_gamma2".format(block_num))(style_tensor)
+    gamma = Reshape([1, 1, filters], name="block_{0}_gamma2_reshape".format(block_num))(beta)
+
+    # Learn a bias for the noise at this level
+    noise = Conv2D(filters=filters,
+                   kernel_size=1,
+                   padding='same',
+                   kernel_initializer='he_normal',
+                   name="block_{0}_noise_bias2".format(block_num)
+                   )(noise_image)
+
+    x = Conv2D(filters=filters,
+               kernel_size=3,
+               padding='same',
+               kernel_initializer='he_normal',
+               name="block_{0}_decoder_conv2".format(block_num)
+               )(x)
+    x = AdaInstanceNormalization(name="block_{0}_AdaIN_2".format(block_num))([x, beta, gamma])
+    x = Add()([x, noise])
+    x = LeakyReLU(name="block_{0}_decoder_LRelu2".format(block_num))(x)
+
+    return x
+
+
 
 class BilinearUpSample(Lambda):
     """
@@ -179,4 +273,68 @@ class BilinearDownSample(Lambda):
         return f"<Keras CustomLayer - BiLinearDownSample - {self.name}>"
 
 
+class AdaInstanceNormalization(Layer):
+    """
+    This is the AdaInstanceNormalization layer used by
+
+    manicman199 available at https://github.com/manicman1999/StyleGAN-Keras
+
+    """
+
+    def __init__(self,
+                 axis=-1,
+                 momentum=0.99,
+                 epsilon=1e-3,
+                 center=True,
+                 scale=True,
+                 **kwargs):
+        super(AdaInstanceNormalization, self).__init__(**kwargs)
+        self.axis = axis
+        self.momentum = momentum
+        self.epsilon = epsilon
+        self.center = center
+        self.scale = scale
+
+    def build(self, input_shape):
+
+        dim = input_shape[0][self.axis]
+        if dim is None:
+            raise ValueError('Axis ' + str(self.axis) + ' of '
+                                                        'input tensor should have a defined dimension '
+                                                        'but the layer received an input with shape ' +
+                             str(input_shape[0]) + '.')
+
+        super(AdaInstanceNormalization, self).build(input_shape)
+
+    def call(self, inputs, training=None):
+        input_shape = K.int_shape(inputs[0])
+        reduction_axes = list(range(0, len(input_shape)))
+
+        beta = inputs[1]
+        gamma = inputs[2]
+
+        if self.axis is not None:
+            del reduction_axes[self.axis]
+
+        del reduction_axes[0]
+        mean = K.mean(inputs[0], reduction_axes, keepdims=True)
+        stddev = K.std(inputs[0], reduction_axes, keepdims=True) + self.epsilon
+        normed = (inputs[0] - mean) / stddev
+
+        return normed * gamma + beta
+
+    def get_config(self):
+        config = {
+            'axis': self.axis,
+            'momentum': self.momentum,
+            'epsilon': self.epsilon,
+            'center': self.center,
+            'scale': self.scale
+        }
+        base_config = super(AdaInstanceNormalization, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+    def compute_output_shape(self, input_shape):
+
+        return input_shape[0]
 
--- introvae/models.py
+++ introvae/models.py
@@ -13,8 +13,8 @@ import tensorflow as tf
 # MODEL SETTINGS
 IMG_DIM = 128
 Z_DIM = 100
-FILTERS = [16, 32, 64, 128]
-BASE_DIM = (4, 4, 128)
+FILTERS = [16, 32, 64, 128, 256]
+BASE_DIM = (4, 4, 256)
 N_LAYERS = len(FILTERS)
 
 # Build Encoder
@@ -34,23 +34,35 @@ for i in range(N_LAYERS):
 z, mu, log_var = add_vae_layer(x, Z_DIM)
 
 # Build Model
-encoder = Model(inputs=[encoder_input],
-                outputs=[z, mu, log_var],
-                name="encoder")
+encoder = Model(inputs=[encoder_input], outputs=[z, mu, log_var], name="encoder")
 
 encoder.summary()
 # ------------------------------------------------------------------------ #
 
 # Build Decoder
 generator_input = Input(shape=(Z_DIM,), name="generator_input")
+z = generator_input
+
+# Crop the noise images
+noise_input = Input(shape=(IMG_DIM, IMG_DIM, 1), name="noise_image")
+noise_img = noise_input
+noise_images = [noise_img]
+curr_size = IMG_DIM
+while curr_size > 8:
+    curr_size = int(curr_size / 2)
+    cut = int(curr_size / 2)
+    crop = Cropping2D(cut)(noise_img)
+    noise_img = crop
+    noise_images.insert(0, crop)
+
+w = add_mapping_layers(z, Z_DIM, 5)
+constant_input, x = add_base_layer(z=False, base_dim=BASE_DIM, style=True)
 
 rgbs = []
-
-x = add_base_layer(generator_input, BASE_DIM)
-
 for i in range(N_LAYERS):
-    rgb = tRGB(x, block=0)
-    x = conv_block(x, FILTERS[::-1][i], block=i, down=False, residual=False)
+    rgb = tRGB(x, block=i)
+    x = style_block(FILTERS[::-1][i], x, w, noise_images[i], i)
+    #x = conv_block(x, FILTERS[::-1][i], block=i, down=False, residual=False)
     rgbs.append(rgb)
 
 # Get final RGB and then sum
@@ -66,7 +78,8 @@ for i, rgb in enumerate(rgbs[1::]):
     img = SUM([img, rgb])
 
 generator_output = img
-generator = Model(inputs=[generator_input], outputs=[generator_output], name="generator")
+generator = Model(inputs=[generator_input, noise_input, constant_input],
+                  outputs=[generator_output], name="generator")
 
 generator.summary()
 
